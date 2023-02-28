a480a26bcafef0d673c2f5b51bd45aa4b14abc02
Vasiliy Vygnych <vygnych11011777@gmail.com>
2023-02-28 14:41:10 +0300

    added label

--- MyTestFromGit/ViewController.swift
+++ MyTestFromGit/ViewController.swift
@@ -8,9 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private lazy var lanel: UILabel = {
+       let label = UILabel(frame: CGRect(x: 50, y: 50, width: 50, height: 50))
+        label.text = "Hello"
+        label.textColor = .black
+        return label
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(lanel)
         view.backgroundColor = .systemPink
     }
 
